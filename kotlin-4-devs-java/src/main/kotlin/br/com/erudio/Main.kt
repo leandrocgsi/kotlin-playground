69c00c5687cf3e372a14f0d5d7692f5e8264bee4
leandrocgsi <leandrocgsi@gmail.com>
2022-02-26 16:16:04 -0300

    Initial Commit

--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/Main.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/Main.kt
@@ -1,11 +1,5 @@
 package br.com.erudio
 
-/*fun main(args: Array<String>) {
-    println("Hello, World!!!")
-}*/
-
 fun main(args: Array<String>) {
-    val name = if (args.isNotEmpty()) args[0] else "Kotlin World"
-    println(args.any())
-    println("Hello, $name!!!")
+    println("Hello, World!!!")
 }
