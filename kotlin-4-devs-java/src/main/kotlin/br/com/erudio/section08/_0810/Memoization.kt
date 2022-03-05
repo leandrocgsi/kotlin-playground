dfba0fc1858b88369640e959c2903fc84cf70bc8
leandrocgsi <leandrocgsi@gmail.com>
2022-03-05 10:10:37 -0300

    Memoization in Kotlin

---
+++ kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section08/_0810/Memoization.kt
@@ -0,0 +1,26 @@
+package br.com.erudio.section08._0810
+
+import org.funktionale.memoization.memoize
+
+fun main() {
+    var fibonacci: (Long) -> Long = {it}
+
+    fibonacci = { n: Long ->
+        if (n < 2) n else fibonacci(n - 1) + fibonacci(n -2)
+    }
+
+    var m: (Long) -> Long = {it}
+
+    m = { n: Long ->
+        if (n < 2) n else m(n - 1) + m(n -2)
+    }.memoize()
+    println("Calling fib: ${timeElapsed { fibonacci(40)}} ms")
+    println("Calling memoized version: ${timeElapsed { m(40)}} ms")
+}
+
+fun timeElapsed(body: ()-> Unit): Long {
+    val start = System.currentTimeMillis()
+    body()
+    val end = System.currentTimeMillis()
+    return end - start
+}
