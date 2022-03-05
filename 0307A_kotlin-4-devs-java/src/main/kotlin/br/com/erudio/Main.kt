a0d8e580676b0ccf0617aa52066755ef0805b7aa
leandrocgsi <leandrocgsi@gmail.com>
2022-03-05 10:40:29 -0300

    Hello Kotlin

--- 0307A_kotlin-4-devs-java/src/main/kotlin/br/com/erudio/Main.kt
+++
@@ -1,5 +0,0 @@
-package br.com.erudio
-
-fun main(args: Array<String>) {
-    println("Hello, World!!!")
-}
--- kotlin-4-devs-java/src/main/kotlin/br/com/erudio/section03/_0307/HelloWorld.kt
+++
@@ -1,6 +0,0 @@
-package br.com.erudio.section03._0307
-
-fun main(args: Array<String>) {
-    val name = if (args.isNotEmpty()) args[0] else "Kotlin World"
-    println("Hello, $name!!!")
-}
