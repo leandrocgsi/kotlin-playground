600c7422851ebfdace6f60f3cd1025b0789b8574
leandrocgsi <leandrocgsi@gmail.com>
2022-03-12 12:39:11 -0300

    Unit and Integration Tests with JUnit, Testcontainers, REST Assured and Mockito in Kotlin

--- rest-with-spring-boot-and-java-erudio-destination/src/test/kotlin/br/com/erudio/unittests/mockito/services/PersonServicesTest.kt
+++ rest-with-spring-boot-and-java-erudio-destination/src/test/kotlin/br/com/erudio/unittests/mockito/services/PersonServicesTest.kt
@@ -18,11 +18,9 @@ import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.jupiter.MockitoExtension
 import java.util.*
-
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension::class)
